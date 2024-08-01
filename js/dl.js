314c6004f5e66d77a5cba0c024ae999623b844d4
kimsojung03 <akstjd1018@naver.com>
2024-08-01 17:15:39 +0900

    feat:인풋예시

---
+++ js/dl.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $("dl dt").click(function(){
+//         $('+dd',this).slideDown(800).siblings('dd').slideUp(800);
+//     });
+//     $('dl dd').click(function(){
+//         $(this).slideUp(800);
+//     })
+// });
+
+$(function(){
+    $('dl dt').on('click',function(){
+        $('+dd',this).slideDown(800).siblings('dd').slideUp(800);
+    });
+    $('dl dd').on('click',function(){
+        $(this).slideUp(500);
+    })
+}); //$(document).ready(function()같은뜻임 축약형//
