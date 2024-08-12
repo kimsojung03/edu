fc4ad142c038516953e49c728134f8bee3fb51d8
rlathwjd031 <akstjd1018@naver.com>
2024-08-12 16:03:33 +0900

    feat:월요일에 스티키연습

--- popup-0808.js
+++ popup-0808.js
@@ -2,14 +2,14 @@ $(function(){
     $('#open-btn').on('click',function(){
         // $('#popup-box').show(1000);
         // $('#popup-box').fadeIn(1000);
-        // $('#popup-box').slideDown(1000);
-        $('#popup-box').addClass('show');
+        $('#popup-box').slideDown(1000);
+        // $('#popup-box').addClass('show');
     });
     $('#popup-box > span').on('click',function(){
         // $('#popup-box').hide(1000);
         // $('#popup-box').fadeOut(1000);
-        // $('#popup-box').slideUp(1000);
-        $('#popup-box').removeClass('show')
+        $('#popup-box').slideUp(1000);
+        // $('#popup-box').removeClass('show')
     });
     
 });
